a879ccfa5fff5a32c519bdc15b2a430b73cf77a1
Henrique Romero <luishenriquebuenoromero@gmail.com>
2024-02-09 15:08:56 +0000

    variaveis

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -2,6 +2,9 @@ package primeiro_exemplo;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+        int cont = 10;
+        String nome = "Luis";
+        System.out.println("Hello World!");
+        System.out.println(cont + " : " + nome);
+    }    
 }
